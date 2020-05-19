5ccd1793c3ea94522693e9bfb045065ac2a992ca
ganiushina <32090008+ganiushina@users.noreply.github.com>
2020-05-19 12:43:40 +0400

    Добавила Адаптер

---
+++ src/MeteoAdapter.java
@@ -0,0 +1,46 @@
+import java.time.*;
+import java.util.Calendar;
+
+public class MeteoAdapter implements MeteoSensor  {
+
+    SensorTemperature sensorTemperature;
+
+    public MeteoAdapter(SensorTemperature sensorTemperature) {
+        this.sensorTemperature = sensorTemperature;
+    }
+
+    @Override
+    public int getId() {
+        return sensorTemperature.identifier();
+    }
+
+    @Override
+    public Float getTemperature() {
+        return (float)sensorTemperature.temperature();
+    }
+
+    @Override
+    public Float getHumidity() {
+        return null;
+    }
+
+    @Override
+    public Float getPressure() {
+        return null;
+    }
+
+    @Override
+    public LocalDateTime getDateTime() {
+//        LocalDateTime dateSensor;
+//        Calendar c = Calendar.getInstance();
+//        c.set(Calendar.YEAR, sensorTemperature.year());
+//        c.set(Calendar.DAY_OF_YEAR, sensorTemperature.day());
+//        c.set(Calendar.HOUR, sensorTemperature.second());
+//        int month = c.get(Calendar.MONTH);
+//        int hour = c.get(Calendar.HOUR);
+//
+//        dateSensor = LocalDateTime.of(sensorTemperature.year(), month, sensorTemperature.day(), hour, sensorTemperature.second());
+//        return dateSensor;
+        return null;
+    }
+}
